6022823bc1a98720830bd9aca74f96eba663bc0d
Daniil <danya_livitin@mail.ru>
2021-07-19 00:54:51 +0300

    Some comments about previous commit

--- src/main/java/dlivitin/springmvcapp/HelloController.java
+++ src/main/java/dlivitin/springmvcapp/HelloController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    // Controller will use this method when user will request localhost:8080/hello-world
     @GetMapping("/hello-world")
     public String sayHello(){
-        return "hello_wolrd.html";
+
+        // We will return hello_world.html page from WEB-INF/views/
+        return "hello_wolrd";
     }
 }
